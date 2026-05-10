7e518d7f791998bf9488df2377e94b034a1261f6
Akira Hayashi <rindrics@gmail.com>
2026-05-10 11:41:43 +0900

    fix: build error

--- lib/repositories/interfaces/lifePlanFamilyMemberRepository.ts
+++ lib/repositories/interfaces/lifePlanFamilyMemberRepository.ts
@@ -1,6 +1,8 @@
 import type { ライフプラン家族メンバー } from './lifePlanRepository';
 import type { SupabaseClient } from '@supabase/supabase-js';
 
+export type { ライフプラン家族メンバー };
+
 export interface ライフプランファミリーメンバーRepository {
   ライフプランID別取得(lifePlanId: string): Promise<ライフプラン家族メンバー[]>;
   複数作成(
